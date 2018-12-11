d15b151ecc5d6b01f0b37253d2ce9ea51938092d
pgrech2 <pgrech2@gmail.com>
2018-12-11 14:16:12 -0500

    Day 2

--- src/com/pgrech2/aoc/day2.clj
+++ src/com/pgrech2/aoc/day2.clj
@@ -1,1 +1,26 @@
 (ns com.pgrech2.aoc.day2)
+
+
+(defn find-boxes [boxids]
+  (->> (frequencies boxids)
+       (filter (comp #{2 3} last))))
+
+(defn checksum [input]
+  (->> (map find-boxes input)
+       (keep seq)
+       (mapcat (comp set (partial map (partial last))))
+       (group-by identity)
+       (map (comp count last))
+       (apply *)))
+
+
+;; Messy Solution, can definitely be refactored a bit
+(defn exercise []
+  (let [input [["a" "b" "c" "d" "e" "f"]
+               ["b" "a" "b" "a" "b" "c"]
+               ["a" "b" "b" "c" "d" "e"]
+               ["a" "b" "c" "c" "c" "d"]
+               ["a" "a" "b" "c" "d" "d"]
+               ["a" "b" "c" "d" "e" "e"]
+               ["a" "b" "a" "b" "a" "b"]]]
+    (checksum input)))
